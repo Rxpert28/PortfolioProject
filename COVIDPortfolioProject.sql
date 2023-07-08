d724c0c1a6df2382ea6d4500bace8682954c6888
Rxpert28 <138037739+Rxpert28@users.noreply.github.com>
2023-07-08 12:20:34 -0400

    Format COVIDPortfolioProject.sql

--- COVIDPortfolioProject.sql
+++ COVIDPortfolioProject.sql
@@ -1,18 +1,19 @@
---Update PortfolioProject..CovidDeathsCSV
---SET continent = NULLIF(continent,'')
---where continent = ''
+-- Update PortfolioProject..CovidDeathsCSV
+UPDATE PortfolioProject..CovidDeathsCSV
+SET continent = NULLIF(continent, '')
+WHERE continent = ''
 
-Select *
-From PortfolioProject..CovidDeathsCSV
-where location = 'world'
-order by 3,4
+-- Select all rows from PortfolioProject..CovidDeathsCSV where location is 'world' and order by column 3 and 4
+SELECT *
+FROM PortfolioProject..CovidDeathsCSV
+WHERE location = 'world'
+ORDER BY 3, 4
 
-Select Location, date, total_cases, new_cases, total_deaths, population
-From PortfolioProject..CovidDeathsCSV
+-- Select specific columns from PortfolioProject..CovidDeathsCSV where location is 'world'
+SELECT Location, date, total_cases, new_cases, total_deaths, population
+FROM PortfolioProject..CovidDeathsCSV
 
--- Total Cases vs Total Deaths per Country
--- Shows likelihood of dying if you get covid in Canada
-
+-- Calculate death percentage in Canada based on total cases and total deaths
 SELECT
     Location,
     Date,
@@ -20,13 +21,11 @@ SELECT
     total_deaths,
     (CAST(total_deaths AS DECIMAL(20, 2)) / CAST(total_cases AS DECIMAL(20, 2))) * 100 AS DeathPercentage
 FROM PortfolioProject..CovidDeathsCSV
-where location = 'Canada'
-and continent is not null
-order by 1,2
+WHERE location = 'Canada'
+AND continent IS NOT NULL
+ORDER BY 1, 2
 
---Looking at Total Cases vs Population
---Shows what percentage of population got COVID
-
+-- Calculate the percentage of population infected with COVID in Canada
 SELECT
     Location,
     Date,
@@ -34,103 +33,102 @@ SELECT
     Population,
     (CAST(total_cases AS DECIMAL(20, 2)) / CAST(population AS DECIMAL(20, 2))) * 100 AS PercentOfPopulationInfected
 FROM PortfolioProject..CovidDeathsCSV
-where location = 'Canada'
-order by 1,2
+WHERE location = 'Canada'
+ORDER BY 1, 2
 
---Countries with highest infection rate compared to population
-
+-- Get countries with the highest infection rate compared to population
 SELECT
     Location,
     Population,
-    MAX(total_cases) as HighestInfectionCount,
-    MAX(CAST(total_cases AS DECIMAL (20,2))/ CAST(population AS DECIMAL(20, 2)))*100 AS PercentPopulationInfected
+    MAX(total_cases) AS HighestInfectionCount,
+    MAX(CAST(total_cases AS DECIMAL(20, 2)) / CAST(population AS DECIMAL(20, 2))) * 100 AS PercentPopulationInfected
 FROM PortfolioProject..CovidDeathsCSV
---where location = 'Canada'
-Group by Location, Population
-order by 4 desc
+GROUP BY Location, Population
+ORDER BY 4 DESC
 
-
--- Countries with highest death count per population
-
+-- Get countries with the highest death count per population
 SELECT
     Location,
-    MAX(CAST(Total_deaths as int)) as TotalDeathCount
+    MAX(CAST(Total_deaths AS INT)) AS TotalDeathCount
 FROM PortfolioProject..CovidDeathsCSV
---where location = 'Canada'
-where continent is not null
-Group by Location
-order by TotalDeathCount desc
+WHERE continent IS NOT NULL
+GROUP BY Location
+ORDER BY TotalDeathCount DESC
 
-
--- BY CONTINENT
-
--- Continents with highest death count
-
+-- Get continents with the highest death count
 SELECT
     continent,
-    MAX(CAST(Total_deaths as int)) as TotalDeathCount
+    MAX(CAST(Total_deaths AS INT)) AS TotalDeathCount
 FROM PortfolioProject..CovidDeathsCSV
---where location = 'Canada'
-where continent is not null
-Group by continent
-order by TotalDeathCount desc
+WHERE continent IS NOT NULL
+GROUP BY continent
+ORDER BY TotalDeathCount DESC
 
--- GLOBAL NUMBERS
-
+-- Get global numbers for new cases, new deaths, and death percentage
 SELECT
     Date,
-    SUM(new_cases) as total_new_cases,
-	SUM(new_deaths) as total_new_deaths,
-	CASE
+    SUM(new_cases) AS total_new_cases,
+    SUM(new_deaths) AS total_new_deaths,
+    CASE
         WHEN SUM(new_cases) = 0 THEN NULL
         ELSE SUM(new_deaths) * 100.0 / SUM(new_cases)
     END AS DeathPercentage
 FROM PortfolioProject..CovidDeathsCSV
-where continent is not null
-Group By Date
-order by 1,2
+WHERE continent IS NOT NULL
+GROUP BY Date
+ORDER BY 1, 2
 
+-- Get total new cases, total new deaths, and death percentage
 SELECT
-    SUM(new_cases) as total_new_cases,
-	SUM(new_deaths) as total_new_deaths,
-	SUM(CASE
+    SUM(new_cases) AS total_new_cases,
+    SUM(new_deaths) AS total_new_deaths,
+    SUM(CASE
         WHEN SUM(new_cases) = 0 THEN NULL
         ELSE SUM(new_deaths) * 100.0 / SUM(new_cases)
     END) AS DeathPercentage
 FROM PortfolioProject..CovidDeathsCSV
-where continent is not null
-Group By Date
-order by total_new_cases, total_new_deaths
+WHERE continent IS NOT NULL
+GROUP BY Date
+ORDER BY total_new_cases, total_new_deaths
 
+-- Join deaths and vaccinations tables
+SELECT *
+FROM PortfolioProject..CovidDeathsCSV dea
+JOIN PortfolioProject..CovidVaccinationsCSV vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
 
--- JOINING DEATHS AND VACCINATIONS TABLES
+-- Get total population vs vaccinations
+SELECT
+    dea.continent,
+    dea.location,
+    dea.date,
+    dea.population,
+    vac.new_vaccinations,
+    SUM(CAST(vac.new_vaccinations AS BIGINT)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.Date) RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeathsCSV dea
+JOIN PortfolioProject..CovidVaccinationsCSV vac
+    ON dea.location = vac.location
+    AND dea.date = vac.date
+WHERE dea.continent IS NOT NULL
+ORDER BY 2, 3
 
-Select *
-From PortfolioProject..CovidDeathsCSV dea
-Join PortfolioProject..CovidVaccinationsCSV vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-
---Looking at Total Population vs Vaccinations
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CAST(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.Date) RollingPeopleVaccinated
-From PortfolioProject..CovidDeathsCSV dea
-Join PortfolioProject..CovidVaccinationsCSV vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-Where dea.continent is not null
-order by 2,3
-
---USING CTE
-
-With PopVsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
-as
+-- Using CTE for population vs vaccinations
+WITH PopVsVac (Continent, Location, Date, Population, New_Vaccinations, RollingPeopleVaccinated)
+AS
 (
-Select dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(CAST(vac.new_vaccinations as bigint)) OVER (Partition by dea.location Order by dea.location, dea.Date) RollingPeopleVaccinated
-From PortfolioProject..CovidDeathsCSV dea
-Join PortfolioProject..CovidVaccinationsCSV vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-Where dea.continent is not null
+    SELECT
+        dea.continent,
+        dea.location,
+        dea.date,
+        dea.population,
+        vac.new_vaccinations,
+        SUM(CAST(vac.new_vaccinations AS BIGINT)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.Date) RollingPeopleVaccinated
+    FROM PortfolioProject..CovidDeathsCSV dea
+    JOIN PortfolioProject..CovidVaccinationsCSV vac
+        ON dea.location = vac.location
+        AND dea.date = vac.date
+    WHERE dea.continent IS NOT NULL
 )
-Select *, (CAST(RollingPeopleVaccinated as bigint)/CAST(Population as bigint))*100
-FROM PopVsVac
+SELECT *, (CAST(RollingPeopleVaccinated AS BIGINT) / CAST(Population AS BIGINT)) * 100
+FROM PopVsVac;
